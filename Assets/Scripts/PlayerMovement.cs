ac60c157024e68618ed1190cf29eb71d72a8249d
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-03-17 21:19:16 -0500

    Added all files, created PlayerMovement.cs script.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Define the rigidbody (rb)
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+    }
+}
