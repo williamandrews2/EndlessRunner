ba6116bfcb70b9f3213ee6b79d269fdc6cfa0ac8
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-03-24 21:14:55 -0500

    Added UI components for updating amount of coins collected on the Canvas.

--- Assets/Scripts/ItemCollection.cs
+++ Assets/Scripts/ItemCollection.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollection : MonoBehaviour
 {
+    [SerializeField] Text coinsText;
+    int coinTotal = 0;
+
+    private void Start()
+    {
+        updateCoins();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Coin")){
             Destroy(other.gameObject);
+            coinTotal++;
+            updateCoins();
         }
+    }
+
+    void updateCoins()
+    {
+        coinsText.text = "Coins: " + coinTotal;
     }
 }
