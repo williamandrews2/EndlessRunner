796be3c235bc162d2aecfb905a8fe3cf76050f36
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-03-25 09:52:40 -0500

    Added music to MainScene.

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
+    [SerializeField] AudioSource mainMusic;
+    private void Start()
+    {
+        mainMusic.Play();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Obstacle")
